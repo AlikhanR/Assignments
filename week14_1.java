42b5dbef3fdb303f26ea29eaeeb534151928035c
AlikhanR <104785843+AlikhanR@users.noreply.github.com>
2022-05-03 13:03:15 +0600

    Update week14_1.java

--- week14_1.java
+++ week14_1.java
@@ -8,23 +8,18 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class week14_1 extends Application {
-    // Input stream
     DataInputStream fromServer = null;
 
-    // Create a label
     private Label label = new Label("");
 
-    @Override // Override the start method in the Application class
     public void start(Stage primaryStage) {
 
-        // Create a stack pane
         StackPane pane = new StackPane();
         pane.getChildren().add(label);
 
-        // Crate a scene and place it in the stage
         Scene scene = new Scene(pane, 300, 75);
-        primaryStage.setTitle("Exercise31_04Client"); // Set the stage title
-        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.setTitle("Exercise31_04Client");
+        primaryStage.setScene(scene);
         primaryStage.show(); // Display the stage
 
         try {
